686f2d69c7b122aab239c4ac3227feaf84754750
IVAN MASTRANGE <75397473+IvanMCarval@users.noreply.github.com>
2023-04-03 15:52:05 -0300

    refactor

--- hr-worker/src/main/java/com/example/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/example/hrworker/resources/WorkerResource.java
@@ -22,19 +22,13 @@ import java.util.List;
 public class WorkerResource {
     private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
-    @Value("${test.config}")
-    private String testConfig;
+    //@Value("${test.config}")
+    //private String testConfig;
 
     @Autowired
     private Environment env;
     @Autowired
     private WorkerRepository repository;
-
-    @GetMapping(value = "/configs")
-    public ResponseEntity<Void> getConfigs() {
-        logger.info("CONFIG = " + testConfig);
-        return ResponseEntity.noContent().build();
-    }
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll() {
