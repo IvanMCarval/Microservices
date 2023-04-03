af0af09bc54e683c8d47ee6e17fd3ac8ce3e6bdf
IVAN MASTRANGE <75397473+IvanMCarval@users.noreply.github.com>
2023-04-03 14:02:19 -0300

    Project hr-config-server created

---
+++ gr-config-server/src/main/java/com/example/grconfigserver/GrConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.grconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrConfigServerApplication.class, args);
+	}
+
+}
---
+++ gr-config-server/src/test/java/com/example/grconfigserver/GrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
